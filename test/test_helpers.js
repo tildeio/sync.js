6af4a36abf44402c4388a24dcbc6c6de509caa83
Yehuda Katz <wycats@gmail.com>
2013-04-04 21:22:58 -0400

    Support a count to call expectation

--- test/test_helpers.js
+++ test/test_helpers.js
@@ -1,16 +1,20 @@
 QUnit.config.testTimeout = 200;
 
-export function async(callback, binding) {
-  stop();
+export function async(callback, count) {
+  count = count || 1;
+
+  for (var i=0; i<count; i++) {
+    stop();
+  }
 
   return function() {
     start();
     if (callback) {
-      return callback.apply(binding, arguments);
+      return callback.apply(this, arguments);
     }
   };
 }
 
-export function expectCall(callback, binding) {
-  return async(callback, binding);
+export function expectCall(callback, count) {
+  return async(callback, count);
 }
