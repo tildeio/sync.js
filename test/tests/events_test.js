e41880c4ce1cb32a30dad1c4e179ebbc0af89f0a
Yehuda Katz <wycats@gmail.com>
2013-04-04 20:41:56 -0400

    Test events against generic operations

--- test/tests/events_test.js
+++ test/tests/events_test.js
@@ -1,3 +1,4 @@
+import { expectCall } from "test_helpers";
 import { reference } from "sync/reference";
 import { applyToCanonical, applyToBuffer } from "sync/operation";
 import { SetProperty } from "sync/operations/set_property";
@@ -35,11 +36,9 @@ function firedEvents() {
 
 var firedEvent = firedEvents;
 
-function Type() {}
-
 module("Triggered Events", {
   setup: function() {
-    ref = reference(Type, 1);
+    ref = reference(1);
 
     resetEvents();
 
@@ -70,26 +69,52 @@ module("Triggered Events", {
 });
 
 test("When an operation is added to canonical, the canonical:change and buffer:change events are fired", function() {
-  applyToCanonical(ref, new SetProperty('firstName', null, 'Tom'));
+  var operation = {
+    test: expectCall(function() {
+      return true;
+    }),
+
+    apply: expectCall()
+  };
+
+  applyToCanonical(ref, operation);
 
   firedEvent('canonical:change', 'buffer:change');
 });
 
 test("When an operation is added to the buffer, the buffer:change event is fired", function() {
-  applyToBuffer(ref, new SetProperty('firstName', null, 'Tom'));
+  applyToBuffer(ref, {
+    test: expectCall(function() {
+      return true;
+    })
+  });
 
   firedEvent('buffer:change');
 });
 
 test("When a reference is marked as saving, its lifecycle:saving event is fired", function() {
-  applyToBuffer(ref, new SetProperty('firstName', null, 'Tom'));
+  applyToBuffer(ref, {
+    test: expectCall(function() {
+      return true;
+    })
+  });
+
   saving(ref);
 
   firedEvents('buffer:change', 'lifecycle:saving');
 });
 
 test("When a reference is marked as saved, its lifecycle:saved event is fired", function() {
-  applyToBuffer(ref, new SetProperty('firstName', null, 'Tom'));
+  applyToBuffer(ref, {
+    test: expectCall(function() {
+      return true;
+    }),
+
+    apply: expectCall(function(snapshot) {
+      deepEqual(snapshot, {}, "The initial state of the canonical is empty");
+    })
+  });
+
   saving(ref);
 
   firedEvents('buffer:change', 'lifecycle:saving');
@@ -102,19 +127,62 @@ test("When a reference is marked as saved, its lifecycle:saved event is fired", 
 });
 
 test("When a canonical is modified for a property with an operation in the buffer, its buffer:change event is not fired", function() {
-  applyToBuffer(ref, new SetProperty('firstName', null, 'Tom'));
+  // This operation is going to accept a transformation that will convert it into a noop.
+  // Converting an operation into a noop does not result in triggering buffer:change,
+  // because the snapshot of the buffer hasn't changed.
+  applyToBuffer(ref, {
+    meta: 'op1',
 
+    // Check to make sure that the operation passed into the transform function is
+    // the operation applied to the canonical.
+    transform: expectCall(function(op) {
+      strictEqual(op.meta, 'op2', "The operation to transform against is op2");
+    }),
+
+    // When asked, tell sync.js that the operation is now a noop.
+    noop: expectCall(function() {
+      return true;
+    })
+  });
+
+  // Initially applying the operation to the buffer triggers a change.
   firedEvents('buffer:change');
 
   resetEvents();
 
-  applyToCanonical(ref, new SetProperty('firstName', null, 'Thomas'));
+  // Apply an operation to the reference. This operation will cancel out the operation
+  // in the buffer, making it a noop.
+  applyToCanonical(ref, {
+    meta: 'op2',
+
+    // Tell sync.js that this operation is compatible with the operation already
+    // in the buffer.
+    isCompatible: function(op) {
+      strictEqual(op.meta, 'op1', "Checking compatibility with op1");
+      return true;
+    },
+
+    // Check to make sure that the canonical snapshot is empty, as expected.
+    apply: expectCall(function(snapshot) {
+      deepEqual(snapshot, {}, "The snapshot is empty so far");
+    })
+  });
 
   firedEvents('canonical:change', 'buffer:transformed');
 });
 
-test("When a canonical is modified for a property without an operation in the buffer, its buffer:change event is not fired", function() {
-  applyToCanonical(ref, new SetProperty('firstName', null, 'Tom'));
+test("When a canonical is modified for a property without an operation in the buffer, its buffer:change event is fired", function() {
+  applyToCanonical(ref, {
+    apply: expectCall()
+  });
 
   firedEvents('canonical:change', 'buffer:change');
+});
+
+test("When a canonical's modification moots the last operation in the buffer, it is no longer dirty", function() {
+  expect(0);
+});
+
+test("TODO: When a canonical's modification moots the last operation in the in-flight, it ...", function() {
+  expect(0);
 });
